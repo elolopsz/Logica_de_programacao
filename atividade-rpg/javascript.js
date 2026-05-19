6aff15ec6d6f9ed866e9ef6950258afb3d8414b3
elolopsz <eloalopes5678@gmail.com>
2026-05-19 12:18:29 -0300

    javasjogo

---
+++ atividade-rpg/javascript.js
@@ -0,0 +1,35 @@
+class personagem {
+    constructor(nome, titulo, hp, mana, energia) {
+        this.nome = nome;
+        this.titulo = titulo;
+        this.hp = hp;
+        this.mana = mana;
+        this.energia = energia;
+    }                            
+}
+class ataque {
+    constructor(id, nome, dano, custo, energia) {
+        this.id = id;
+        this.nome = nome;
+        this.dano = dano;
+        this.custo = custo;
+        this.energia = energia;
+    }
+}
+// instancia classes - criar objetos
+let hero = new personagem("R-01", "A arqueira",100, 100, 0);
+let boss = new personagem("azog", "O guerreiro polvo", 100, 0, 50);
+// preencher os status 
+document
+getElementById("nome-hero")
+textContent = hero.nome;
+document
+.getElementById("titulo-hero")
+.textContent = hero.titulo;
+// criar habilidades
+let listaHabilidades = [
+    new Habilidade(1, " 🎯Tiro certeiro", 20, 10, 0),
+    new Habilidade(2, "💥 Explosão de flechas", 30, 20, 0),
+    new Habilidade(3, "🏹 chuva de flechas", 40, 30, 0)
+
+];
